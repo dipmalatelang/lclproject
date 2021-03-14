064687ae93854d1bafa9f2c6b03dd5023e95b73c
Dipmala <telangdipmala@gmail.com>
2021-03-14 18:21:21 +0530

    home screen design

--- app/src/main/java/com/example/lcl/view/HomeScreen_Activity.java
+++ app/src/main/java/com/example/lcl/view/HomeScreen_Activity.java
@@ -1,16 +1,34 @@
 package com.example.lcl.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.lcl.R;
+import com.example.lcl.databinding.ActivityHomeScreenBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeScreen_Activity extends AppCompatActivity {
+    ActivityHomeScreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
+//        setContentView(R.layout.activity_home_screen);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home_screen);
+        initNavigation();
+    }
+    private void initNavigation() {
+        BottomNavigationView bottomNavigationView = binding.bottomNavigationView;
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_home_nav_host);
+
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
--- app/src/main/java/com/example/lcl/view/login/LoginActivity.java
+++ app/src/main/java/com/example/lcl/view/login/LoginActivity.java
@@ -18,7 +18,7 @@ public class LoginActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+//        setContentView(R.layout.activity_login);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
